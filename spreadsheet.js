9e716e6491d28307f31de1ea17057639cccaed6a
dancxjo <tdreed@gmail.com>
2013-09-18 09:02:47 -0700

    Update spreadsheet.js

--- spreadsheet.js
+++ spreadsheet.js
@@ -1,3 +1,27 @@
-function Table(data) {
+function Table(tableId, data, headings) {
+  this.table = document.getElementById(tableId);
   this.data = data;
+  this.headings = headings;
+  
+  this.clear = function () {
+    for (var index=this.table.rows.length - 1; index >= 0; index--) {
+			this.table.deleteRow(index);
+		}
+  };
+  
+  this.makeHeadings = function () {
+    var row = this.table.createTHead(0);
+    for (var index in this.headings) {
+      alert(index);
+      var cell = row.insertCell(index);
+      cell.innerHTML = this.headings[index];
+    }
+  }
+  
+  this.update = function () {
+    this.clear(); // Erase what's already in the table
+    this.makeHeadings(); // Make the headingings
+  };
+  
+  this.update(); // Go ahead and fill the table with data
 }
