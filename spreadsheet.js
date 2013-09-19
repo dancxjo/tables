4cf03f72dbbbc96ec40ee0134e9db7c52f5b2367
dancxjo <tdreed@gmail.com>
2013-09-18 17:13:22 -0700

    Update spreadsheet.js

--- spreadsheet.js
+++ spreadsheet.js
@@ -178,7 +178,6 @@ function Table(tableId, data, headings) {
       for (var varName in this.data[row]) {
         vars += "var " + varName + "=this."+varName + ".content; ";
       }
-      alert(vars);
       return eval(vars + ";\n" + content.substr(1));
     }
     return content;
